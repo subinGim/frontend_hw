40bb2dbc0f3c9cac7778ccbd7d21ef09e21b20c2
Gim, subin <vk0218@naver.com>
2020-03-26 21:22:36 +0900

    initial commit

---
+++ task01/pages.js
@@ -0,0 +1,3 @@
+let studentCount = 31;
+let pageSize = 10;
+console.log(parseInt(studentCount/pageSize)+1);
---
+++ task01/random.js
@@ -0,0 +1,5 @@
+let count = 0;
+while(count < 30){
+    console.log(Math.floor(Math.random()*11+10));
+    count++;
+}
